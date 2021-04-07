2f8accf3e8e8f6019f6002f0762a25f2d5c22855
LiSplib <jcbarret@outlook.fr>
2021-04-07 17:19:36 +0200

    edit landing page

---
+++ src/Controller/AppController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('home.html.twig');
+    }
+
+}
